42d1b6d47daccdd5d0b86d45d2d15a7fecd7fea4
vv <valera.kostin@gmail.com>
2016-12-03 00:13:02 +0100

    change java class

--- src/utils/Utils.kt
+++ src/utils/Utils.kt
@@ -4,7 +4,7 @@ import java.nio.file.Files
 import java.nio.file.Paths
 
 fun getLinesFromResources(name: String): List<String> {
-    val url = Object::class.java.classLoader.getResource(name)
+    val url = String.javaClass.classLoader.getResource(name)
     if (url != null) {
         val lines = Files.readAllLines(Paths.get(url.toURI()))
         return lines
