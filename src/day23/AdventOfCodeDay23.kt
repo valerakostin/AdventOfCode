4fc2b61060692fe6ea0292c50d63841d429aa86f
vv <valera.kostin@gmail.com>
2016-12-23 16:46:52 +0100

    day23 solution, extract processor, reactor day12

--- src/day23/AdventOfCodeDay23.kt
+++ src/day23/AdventOfCodeDay23.kt
@@ -1,4 +1,4 @@
-package day12
+package day23
 
 import utils.MonorailProcessor
 import utils.getLinesFromResources
@@ -6,21 +6,24 @@ import utils.getLinesFromResources
 
 fun solution() {
 
-    val lines = getLinesFromResources("InputDay12.txt")
-    val processor = MonorailProcessor()
+    val lines = getLinesFromResources("InputDay23.txt")
+    val processor = MonorailProcessor(mapOf("a" to 7))
     processor.executeProgram(lines)
     val result1 = processor.getRegisterValue("a")
     println("Task1 $result1")
 
-    val processor2 = MonorailProcessor()
-    val lines2 = listOf("cpy 1 c") + lines
+
+    val lines2 = getLinesFromResources("InputDay231.txt")
+
+    val processor2 = MonorailProcessor(mapOf("a" to 12))
     processor2.executeProgram(lines2)
     val result2 = processor2.getRegisterValue("a")
     println("Task2 $result2")
+
 }
 
 fun main(argc: Array<String>) {
 
-    println("Day12:")
+    println("Day23:")
     solution()
 }
---
+++ src/utils/MonorailProcessor.kt
@@ -0,0 +1,149 @@
+package utils
+
+typealias Program = List<String>
+
+class MonorailProcessor(initialState: Map<String, Int> = emptyMap()) {
+
+    var registers = mutableMapOf<String, Int>()
+
+    init {
+        registers.putAll(initialState)
+    }
+
+    var programCounter: Int = 0
+
+    fun executeProgram(program: Program) {
+
+        val commands = readProgram(program)
+        while (programCounter < program.size) {
+
+            val command = commands[programCounter]
+
+            // println(registers)
+            //  println(program[programCounter])
+
+            when (command) {
+                is Command.Inc -> {
+                    registers[command.source] = registers.getOrDefault(command.source, 0) + 1
+                    programCounter++
+                }
+                is Command.Dec -> {
+                    registers[command.source] = registers.getOrDefault(command.source, 0) - 1
+                    programCounter++
+                }
+                is Command.Cpy -> {
+                    val value = if (command.value.isRegister()) registers.getOrDefault(command.value, 0) else command.value.toInt()
+                    registers[command.source] = value
+                    programCounter++
+                }
+                is Command.Jnz -> {
+
+                    val value = if (command.source.isRegister()) registers.getOrDefault(command.source, 0) else command.source.toInt()
+                    if (value != 0) {
+                        val value2 = if (command.value.isRegister()) registers.getOrDefault(command.value, 0) else command.value.toInt()
+                        programCounter += value2
+                    } else {
+                        programCounter++
+                    }
+                }
+
+                is Command.Tgl -> {
+
+                    val shift = if (command.source.isRegister()) registers.getOrDefault(command.source, 0) else command.source.toInt()
+
+                    val newCounter = programCounter + shift
+
+                    if (newCounter < program.size) {
+
+
+                        val nextCommand = commands[newCounter]
+
+                        if (nextCommand is Command.Inc)
+                            commands[newCounter] = Command.Dec(nextCommand.source)
+                        else if (nextCommand is Command.Dec)
+                            commands[newCounter] = Command.Inc(nextCommand.source)
+                        else if (nextCommand is Command.Tgl)
+                            commands[newCounter] = Command.Inc(nextCommand.source)
+                        else if (nextCommand is Command.Cpy)
+                            commands[newCounter] = Command.Jnz(nextCommand.value, nextCommand.source)
+                        else if (nextCommand is Command.Jnz)
+                            commands[newCounter] = Command.Cpy(nextCommand.value, nextCommand.source)
+                    }
+                    programCounter++
+                }
+                is Command.Nop -> {
+                    programCounter++
+                }
+                is Command.Add -> {
+
+                    val reg1 = if (command.regA.isRegister()) registers.getOrDefault(command.regA, 0) else command.regA.toInt()
+                    val reg2 = if (command.regB.isRegister()) registers.getOrDefault(command.regB, 0) else command.regB.toInt()
+
+
+                    val result = if (reg1 == 0) reg2 else reg1 + reg2
+
+                    if (command.regA.isRegister())
+                        registers[command.regA] = result
+                    registers[command.regB] = 0
+
+                    programCounter++
+                }
+                is Command.Mul -> {
+
+                    val reg1 = if (command.regA.isRegister()) registers.getOrDefault(command.regA, 0) else command.regA.toInt()
+                    val reg2 = if (command.regB.isRegister()) registers.getOrDefault(command.regB, 0) else command.regB.toInt()
+
+                    val result = reg1 * reg2
+
+                    if (command.regA.isRegister())
+                        registers["a"] = result
+                    registers["d"] = 0
+                    registers["c"] = 0
+
+                    programCounter++
+                }
+            }
+        }
+    }
+
+    fun String.isRegister() = all(Char::isLetter)
+
+    fun getRegisterValue(reg: String) = registers.getOrDefault(reg, 0)
+
+
+    sealed class Command(val source: String) {
+
+        class Inc(reg: String) : Command(reg)
+        class Dec(reg: String) : Command(reg)
+        class Cpy(reg: String, val value: String) : Command(reg)
+        class Jnz(reg: String, val value: String) : Command(reg)
+        class Tgl(reg: String) : Command(reg)
+        object Nop : Command("")
+        class Add(val regA: String, val regB: String) : Command("")
+        class Mul(val regA: String, val regB: String) : Command("")
+    }
+
+    companion object {
+
+        fun readProgram(lines: List<String>): MutableList<Command> {
+
+            return lines.map { it.split(" ") }.map {
+
+                when (it[0]) {
+                    "inc" -> Command.Inc(it[1])
+                    "dec" -> Command.Dec(it[1])
+                    "cpy" -> Command.Cpy(it[2], it[1])
+                    "jnz" -> Command.Jnz(it[1], it[2])
+                    "tgl" -> Command.Tgl(it[1])
+                    "nop" -> Command.Nop
+                    "add" -> Command.Add(it[1], it[2])
+                    "mul" -> Command.Mul(it[1], it[2])
+                    else -> {
+                        println("Unknown command ${it[0]}")
+                        Command.Nop
+                    }
+                }
+            }.toMutableList()
+        }
+    }
+}
